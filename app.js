f70b627bc2f17db8a8cd1794a6b60debfd810322
yukino-kikusaki <yukino.kikusaki@azone-japan.co.jp>
2024-04-10 13:20:43 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 'use strict';
+const memo = new Map();
+memo.set(0,0);
+memo.set(1,1);
+
+//nをキーとした答え（数値）をもっていればその値をそのまま返す
+//持っていなければ再帰関数を計算してその答えをMapに格納して返す
 function fib(n){
-
-  if(n === 0){
-    return 0;
-  }else if(n === 1){
-    return 1;
+  if(memo.has(n)){
+    return memo.get(n);
   }
-  
-  return fib(n-1)+fib(n-2);
+  const value = fib(n-1) + fib(n-2);
+  memo.set(n , value);
+  return value;
 }
-
 const length = 40;
 for(let i = 0;i <= length;i++){
   console.log(fib(i));
 }
+
+// function fib(n){
+
+//   if(n === 0){
+//     return 0;
+//   }else if(n === 1){
+//     return 1;
+//   }
+  
+//   return fib(n-1)+fib(n-2);
+// }
+
+// const length = 40;
+// for(let i = 0;i <= length;i++){
+//   console.log(fib(i));
+// }
